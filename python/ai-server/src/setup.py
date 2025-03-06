a1a1312e3581f90d9b44b720c1adcde8a2b64299
Maher Khalil <themaherkhalil@gmail.com>
2025-03-05 19:04:03 -0500

    fix(style): formatting using linter and updated version

--- python/ai-server/src/setup.py
+++ python/ai-server/src/setup.py
@@ -2,27 +2,25 @@ from setuptools import find_packages, setup
 import os
 
 # Read the contents of README.md if it exists
-if os.path.exists('README.md'):
-    with open('README.md', 'r') as f:
+if os.path.exists("README.md"):
+    with open("README.md", "r") as f:
         long_description = f.read()
 else:
     long_description = "AI Server SDK - Python client SDK to connect to the AI Server."
 
 setup(
     name="ai-server-sdk",
-    version="0.0.18",
+    version="0.0.19",
     packages=find_packages(),
     install_requires=[
-        'requests',
-        'pandas',
-        'jsonpickle',
+        "requests",
+        "pandas",
+        "jsonpickle",
     ],
-    extras_require={
-        "full": ["langchain", "langchain-community"]
-    },
+    extras_require={"full": ["langchain", "langchain-community"]},
     author="Thomas Trankle, Maher Khalil, Ryan Weiler",
     description="Utility package to connect to AI Server instances.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     license="MIT",
 )
