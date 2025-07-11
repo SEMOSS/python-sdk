172517ae6d90ff73532eaccb7cfa0dceb61e5ad0
Weiler, Ryan <ryanweiler92@gmail.com>
2025-07-11 14:21:56 -0400

    Clean up & updating import strategy

--- python/ai-server/src/ai_server/__init__.py
+++ python/ai-server/src/ai_server/__init__.py
@@ -2,51 +2,26 @@
 
 import warnings
 from importlib import metadata
-from typing import Any
 
 try:
     __version__ = metadata.version(__package__)
 except metadata.PackageNotFoundError:
-    # Case where package metadata is not available.
     __version__ = "0.0.25"
-del metadata  # optional, avoids polluting the results of dir(__package__)
+del metadata
+
+from .server_resources.server_client import ServerClient
+from .py_client.gaas.model import ModelEngine
+from .py_client.gaas.storage import StorageEngine
+from .py_client.gaas.database import DatabaseEngine
+from .py_client.gaas.vector import VectorEngine
+from .py_client.gaas.function import FunctionEngine
 
 
-def __getattr__(name: str) -> Any:
-    if name == "RESTServer":
-        warnings.warn(
-            "Use of RESTServer is deprecated. Please update your code to use ServerClient instead."
-        )
-
-        from .server_resources.server_client import ServerClient
-
-        return ServerClient
-    elif name == "ServerClient":
-        from .server_resources.server_client import ServerClient
-
-        return ServerClient
-    elif name == "ModelEngine":
-        from .py_client.gaas.model import ModelEngine
-
-        return ModelEngine
-    elif name == "StorageEngine":
-        from .py_client.gaas.storage import StorageEngine
-
-        return StorageEngine
-    elif name == "DatabaseEngine":
-        from .py_client.gaas.database import DatabaseEngine
-
-        return DatabaseEngine
-    elif name == "VectorEngine":
-        from .py_client.gaas.vector import VectorEngine
-
-        return VectorEngine
-    elif name == "FunctionEngine":
-        from .py_client.gaas.function import FunctionEngine
-
-        return FunctionEngine
-    else:
-        raise AttributeError(f"Could not find: {name}")
+def RESTServer(*args, **kwargs):
+    warnings.warn(
+        "Use of RESTServer is deprecated. Please update your code to use ServerClient instead."
+    )
+    return ServerClient(*args, **kwargs)
 
 
 __all__ = [
@@ -56,4 +31,5 @@ __all__ = [
     "StorageEngine",
     "DatabaseEngine",
     "VectorEngine",
+    "FunctionEngine",
 ]
