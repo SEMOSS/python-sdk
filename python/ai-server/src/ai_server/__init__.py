78ca6f750391bd5c49ba10e4c9acc24ae212440f
Ryan Weiler <ryanweiler92@gmail.com>
2025-10-10 12:38:57 -0400

    bumping version

--- python/ai-server/src/ai_server/__init__.py
+++ python/ai-server/src/ai_server/__init__.py
@@ -6,7 +6,7 @@ from importlib import metadata
 try:
     __version__ = metadata.version(__package__)
 except metadata.PackageNotFoundError:
-    __version__ = "0.0.27"
+    __version__ = "0.0.28"
 del metadata
 
 from .server_resources.server_client import ServerClient
